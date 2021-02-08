d9173abd2e4d175a93364dd6f882da8f9edb85eb
nsms556 <nsms556@kakao.com>
2021-02-09 01:22:12 +0900

    modify saving Weight

--- run.py
+++ run.py
@@ -11,11 +11,12 @@ import Snake_Statics as Static
 
 pygame.init()
 
-#nets = [SnakeNet(pre_weight='./Snake/BW1.npy') for _ in range(N_POPULATION)]
+#nets = [SnakeNet(pre_weight='./Snake/BW.npy') for _ in range(N_POPULATION)]
 nets = [SnakeNet() for _ in range(N_POPULATION)]
-best = None
 fitness_list = []
-
+best = None
+bw = None
+bf = 0
 generation = 0
 
 try :
@@ -37,6 +38,9 @@ try :
         fitness_list.append(nets[0].fitness)
 
         best = deepcopy(nets[:N_BEST])
+        if nets[0].fitness >= bf :
+            bw = deepcopy(nets[0].weights)
+            bf = nets[0].fitness
 
         for i in range(N_CHILDREN):
             aParent = random.choice(best)
@@ -49,7 +53,8 @@ try :
         for i in range(int(N_POPULATION / (N_BEST + N_CHILDREN))):
             for model in best:
                 #child = model.mutate_best_base(best[0], MUTATION_RATE)
-                child = model.mutate_Gaussian(MUTATION_RATE)
+                #child = model.mutate_Gaussian(MUTATION_RATE)
+                child = model.mutate_Random(MUTATION_RATE)
                 nets.append(child)        
 
 except Static.BreakException :
@@ -61,4 +66,4 @@ print('Save Best Weight ? (y/n)')
 saved = input()
 
 if saved == 'y' :
-    np.save('./Snake/BW', best[0].weights)
+    np.save('./Snake/BW', bw)
