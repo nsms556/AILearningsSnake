e59cc78e05a6573db46846a574fb2ab145855f2b
nsms556 <nsms556@kakao.com>
2021-03-24 21:34:04 +0900

    class rename

--- models/torchModel.py
+++ models/torchModel.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-class Qnet(nn.Module) :
+class QNet(nn.Module) :
     def __init__(self, input_size, hidden_size, output_size) :
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
